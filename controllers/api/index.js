3f09c53dea31d45b9a8a064161b5fe86d6e3cacf
CL <cl.czayas@gmail.com>
2023-01-18 20:55:28 -0400

    Added create user route, fixed connection, imported login/logout js & login handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 router.use("/users", userRoutes);
 
-// This is the 'get' route
-router.get("/", async (req, res) => {
-  // Here, index.html is rendered
-  res.sendFile(path.join(__dirname, "../views/index.html"));
-});
+// // This is the 'get' route
+// router.get("/", async (req, res) => {
+//   // Here, index.html is rendered
+//   res.sendFile(path.join(__dirname, "../views/index.html"));
+// });
 
 module.exports = router;
