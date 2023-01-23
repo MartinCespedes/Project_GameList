1904c2a49191062fb3f9f999f3498d60d96ce66e
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-23 14:18:56 -0500

    Created route for news & about, as well as handlebars template

--- server.js
+++ server.js
@@ -1,62 +1,45 @@
-// Dependencies
-const express = require('express');
-const path = require('path');
-
-// Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static(path.join(__dirname, 'public')));
-// Sets up the routes
-app.use(require('/'));
-
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
-});
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
-const helpers = require("./utils/helpers");
-const sequelize = require("./config/connection");
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const routes = require('./controllers');
+const routes = require("./controllers");
 // const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 
 // Create a new sequelize store using the express-session package
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 // Configure and link a session object with the sequelize store
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: "Super secret secret",
+  cookie: {
+    // maxAge Attribute defines how long the data (cookie) is stored for//
+    maxAge: 60 * 60 * 1000,
+    sameSite: "strict",
+    httpOnly: true,
+    secure: false,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 // Add express-session and store as Express.js middleware
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
