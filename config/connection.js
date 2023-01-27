aeab42e30e597157ef09538ff299222597fe4e85
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-26 21:42:55 -0500

    Added processs.env

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const { DB_USER, DB_NAME, DB_PASSWORD, JAWSDB_URL } = require("./index");
 
-const sequelize = JAWSDB_URL
-  ? new Sequelize(JAWSDB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
       host: "localhost",
       dialect: "mysql",
