e4e74c7545f007713e73863d36797b0b70d5fbc0
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-26 21:37:41 -0500

    Added DB_NAME in index.js

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ module.exports = {
   PORT: process.env.PORT,
   API_KEY: process.env.API_KEY,
   DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   JAWSDB_URL: process.JAWSDB_URL,
 };
